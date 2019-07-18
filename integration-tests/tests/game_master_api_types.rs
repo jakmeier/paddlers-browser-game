f1b5ea88f30d70d62675db5d014f3814e8d9d8b5
jakmeier <jakmeier@student.ethz.ch>
2019-07-18 22:49:45 +0200

    Delete redundant tests

--- integration-tests/tests/game_master_api_types.rs
+++ integration-tests/tests/game_master_api_types.rs
@@ -1,40 +1,5 @@
-//! Tests to ensure API types (compatible with Diesel and Juniper) are aligned with API types (compatible with WASM)
+//! Has become redundant with the merge of the two libraries
 
 #[test]
 fn test_building_type() {
-    use paddlers_shared_lib::strum::IntoEnumIterator;
-    let mut db_data_iter = paddlers_shared_lib::models::BuildingType::iter();
-    for browser_data in paddlers_api_lib::types::BuildingType::iter() {
-
-        let msg = serde_json::to_string(&browser_data);
-        assert!(msg.is_ok(), "Serialization failed");
-
-        let parsed = serde_json::from_str::<paddlers_shared_lib::models::BuildingType>(&msg.unwrap());
-        assert!(parsed.is_ok(), "API and DB have diverged");
-
-        let db_data = db_data_iter.next();
-        assert!(db_data.is_some(), "Unequal enum member count");
-        assert_eq!(parsed.unwrap(), db_data.unwrap());
-    }
-    assert!(db_data_iter.next().is_none(), "Unequal enum member count");
-}
-
-#[test]
-fn test_resource_type() {
-
-    use paddlers_shared_lib::strum::IntoEnumIterator;
-    let mut db_data_iter = paddlers_shared_lib::models::ResourceType::iter();
-    for browser_data in paddlers_api_lib::types::ResourceType::iter() {
-
-        let msg = serde_json::to_string(&browser_data);
-        assert!(msg.is_ok(), "Serialization failed");
-
-        let parsed = serde_json::from_str::<paddlers_shared_lib::models::ResourceType>(&msg.unwrap());
-        assert!(parsed.is_ok(), "API and DB have diverged");
-
-        let db_data = db_data_iter.next();
-        assert!(db_data.is_some(), "Unequal enum member count");
-        assert_eq!(parsed.unwrap(), db_data.unwrap());
-    }
-    assert!(db_data_iter.next().is_none(), "Unequal enum member count");
 }
