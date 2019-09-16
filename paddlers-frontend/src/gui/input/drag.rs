05287ef02e3f11c8ef7bfa1b0556250096e46d41
jakmeier <jakmeier@student.ethz.ch>
2019-09-16 16:15:58 +0200

    Only drag map in correct view

--- paddlers-frontend/src/gui/input/drag.rs
+++ paddlers-frontend/src/gui/input/drag.rs
@@ -1,8 +1,7 @@
+use crate::game::map::GlobalMapSharedState;
 use quicksilver::prelude::*;
 use specs::prelude::*;
-use crate::game::{
-    map::GlobalMapSharedState,
-};
+use super::{UiState, UiView};
 
 /// Consumes dragging movements and applies them to the game state.
 pub struct DragSystem;
@@ -11,34 +10,43 @@ impl<'a> System<'a> for DragSystem {
     type SystemData = (
         Write<'a, Drag>,
         Write<'a, GlobalMapSharedState>,
+        Read<'a, UiState>,
     );
 
-    fn run(&mut self, 
-        (
-            mut drag,
-            mut map,
-        ): Self::SystemData) 
-    {
+    fn run(&mut self, (mut drag, mut map, ui_state): Self::SystemData) {
         if let Some((start, end)) = drag.0.take() {
-            let v = end - start;
-            map.drag(v * 0.02);
+            let in_menu_area = start.overlaps_rectangle(&(*ui_state).menu_box_area);
+
+            match (ui_state.current_view, in_menu_area) {
+                (UiView::Town, true) => {
+                    // NOP
+                }
+                (UiView::Map, true) => {
+                    // NOP
+                }
+                (UiView::Town, false) => {
+                    // NOP
+                }
+                (UiView::Map, false) => {
+                    let v = end - start;
+                    map.drag(v * 0.02);
+                }
+            }
         }
     }
 }
 
-
 #[derive(Default, Clone, Copy)]
-/// Represent a drag input waiting to be processed by the DragSystem.
-/// Can only hold one drag at the time. 
+/// Represents a drag input waiting to be processed by the DragSystem.
+/// Can only hold one drag at the time.
 /// When more drags are added, they are summarized to one single movement.
-pub struct Drag(Option<(Vector,Vector)>);
+pub struct Drag(Option<(Vector, Vector)>);
 
-impl Drag{
+impl Drag {
     pub fn add(&mut self, start: Vector, end: Vector) {
         if let Some(old) = self.0 {
             self.0 = Some((old.0, end));
-        }
-        else {
+        } else {
             self.0 = Some((start, end));
         }
     }
