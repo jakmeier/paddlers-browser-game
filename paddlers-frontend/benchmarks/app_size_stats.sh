5b315cb447f1b6a74c83bc737fd1330bebfe28c1
jakmeier <jakmeier@student.ethz.ch>
2021-01-04 17:15:41 +0100

    Update app size statistics

--- paddlers-frontend/benchmarks/app_size_stats.sh
+++ paddlers-frontend/benchmarks/app_size_stats.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
 # Run from project root
-DEPLOY_PATH=./target/deploy
+DEPLOY_PATH=./www/dist/
 OUTPUT_PATH=./paddlers-frontend/benchmarks/app_size_stats.txt
 
 cd paddlers-frontend;
-cargo web deploy --release -p paddlers-frontend
+wasm-pack build
+cd ../www;
+npm run release
 cd ..;
 
 cargo pkgid -p paddlers-frontend | cut -d# -f2 | cut -d: -f2 | tr -d '\n' >> $OUTPUT_PATH
 echo -n " " >> $OUTPUT_PATH
-du -sb $DEPLOY_PATH | cut -f 1 | tr -d '\n' >> $OUTPUT_PATH
+# du -sb $DEPLOY_PATH | cut -f 1 | tr -d '\n' >> $OUTPUT_PATH
+echo -n "?" >> $OUTPUT_PATH
 echo -n " " >> $OUTPUT_PATH
-stat --printf="%s" $DEPLOY_PATH/paddlers-frontend.wasm >> $OUTPUT_PATH
+stat --printf="%s" $DEPLOY_PATH/*.wasm >> $OUTPUT_PATH
 echo -n " " >> $OUTPUT_PATH
-stat --printf="%s" $DEPLOY_PATH/paddlers-frontend.js >> $OUTPUT_PATH
+stat -c "%s" $DEPLOY_PATH/*.js | paste -d+ -s - | bc >> $OUTPUT_PATH
 echo "" >> $OUTPUT_PATH
