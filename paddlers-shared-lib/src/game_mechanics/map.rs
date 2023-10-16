81679694c1d830729a816ccc8c3689a293707d0f
Jakob Meier <mail@jakobmeier.ch>
2023-10-17 01:54:52 +0200

    Make more space for player villages
    Short-term patch for #66.
    
    This makes the `streams_to_add_village` a bit less efficient but it
    should still not be an issue.

--- paddlers-shared-lib/src/game_mechanics/map.rs
+++ paddlers-shared-lib/src/game_mechanics/map.rs
@@ -1,5 +1,5 @@
 pub const MAP_H: u32 = 11;
-pub const MAP_MAX_X: u32 = 20;
+pub const MAP_MAX_X: u32 = 1000;
 pub const MAP_STREAM_AREA_W: f32 = 5.0;
 
 pub fn map_distance(a: (f32, f32), b: (f32, f32)) -> f32 {
